adda6c547ca98c4e6842da997d57be272f33e051
Arseniy <seny_2978@mail.ru>
2022-10-06 01:21:10 +0300

    readycalculateusd

--- js/script.js
+++ js/script.js
@@ -5,4 +5,23 @@ const inputUsd = document.querySelector("#usd");
 
 inputRub = addEventListener("input", () => {
   const request = new XMLHttpRequest();
+
+  request.open("GET", "js/current.json");
+  request.setRequestHeader("Content-type", "application/json; charset=utf-8");
+  request.send();
+
+  request.addEventListener("load", () => {
+    if (request.status === 200) {
+      console.log(request.response);
+      const data = JSON.parse(request.response);
+      inputUsd.value = +inputRub.value * data.current.usd;
+    } else {
+      inputUsd.value = "something wrong, any trouble";
+    }
+  });
 });
+
+//status - очевидно, что текущий статус сервера
+//statusText -  тот же статус, но написано буквами, а не цифрами
+//response - типо ответ от сервака
+//readyState
